0ca741febd46bded4669c3503662f9b9b9458022
Rini <rini.vidhya88@gmail.com>
2017-03-16 16:29:47 +0100

    TestNGExample test initial commit

---
+++ testNGBatchFile.bat
@@ -0,0 +1,5 @@
+set projectLocation=C:\Users\Rivi\workspace\TestNGBatchExample
+cd %projectLocation%
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*
+java org.testng.TestNG %projectLocation%\testng.xml
+pause
